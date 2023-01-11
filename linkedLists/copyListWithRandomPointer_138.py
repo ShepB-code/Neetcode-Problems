033886f18b8481de071c61858dd76e01f27a56a6
ShepB-code <shepardjberry@gmail.com>
2023-01-10 19:50:44 -0600

    comitting unsaved changes
    Co-authored-by: Peter Chacko <codeNinjaDev@users.noreply.github.com>
    Co-authored-by: Aimbotpico <Aimbotpico@users.noreply.github.com>

---
+++ linkedLists/copyListWithRandomPointer_138.py
@@ -0,0 +1,21 @@
+
+# Definition for a Node.
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # copy list structure without random
+        hashmap = {}
+        
+        dummy = head
+
+        
+        while dummy:
+            newList = Node(head)
